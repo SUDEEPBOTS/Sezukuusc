6c1c24c1aa2883cb2f0a3c67a8863c56c3170092
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2025-12-03 01:49:20 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
+
+# Important IDs
+LOGGER_ID = -1003444664979
+OWNER_ID = 6356015122
+BOT_USERNAME = "@Sezukuusecuritybot"
