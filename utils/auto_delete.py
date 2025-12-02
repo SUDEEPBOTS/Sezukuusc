aba9e0f9336ba5d5fcd671d964539bd2845a83e3
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2025-12-03 01:47:29 +0530

    Update auto_delete.py

--- utils/auto_delete.py
+++ utils/auto_delete.py
@@ -1,7 +1,11 @@
+# utils/auto_delete.py
+
 import asyncio
 
 async def auto_delete(message):
+    """Deletes any bot message after 3 minutes."""
     await asyncio.sleep(180)
+
     try:
         await message.delete()
     except:
